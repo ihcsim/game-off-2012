51a9348e959b8d448e7779c58456c504659905c1
Ivan Sim <ihcsim@gmail.com>
2012-10-28 19:59:25 -0700

    Add codes to repeat frame animation

--- js/init-game.js
+++ js/init-game.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
       stageCanvasCtx = stageCanvas[0].getContext("2d"),
       stageWidth = stage[0].width,
       stageHeight = stage[0].height,
-      gameStarted = false,
+      isPlaying = false,
       requestAnimFrame = window.requestAnimationFrame ||
                          window.webkitRequestAnimationFrame ||
                          window.mozRequestAnimationFrame ||
@@ -23,12 +23,41 @@ $(document).ready(function(){
   }
   
   function begin(){
-    stageCtx.drawImage(imgSprite, 0, 0, stageWidth, stageHeight, 0, 0, stageWidth, stageHeight);
-    gameStarted = true;
+    var imgStartX = 0;
+    var imgStartY = 0;
+    var imgWidth = stageWidth;
+    var imgHeight = stageHeight;
+    var stageStartX = 0;
+    var stageStartY = 0;
+    
+    stageCtx.drawImage(imgSprite, imgStartX, imgStartY, imgWidth, imgHeight, stageStartX, stageStartY, stageWidth, stageHeight);
+    isPlaying = true;
     requestAnimFrame(loop);
   }
   
   function loop(){
+    if(isPlaying) {
+      update();
+      draw();
+      requestAnimFrame(loop);
+    }
+  }
+  
+  function update(){
+    clearCanvas(stageCanvasCtx);
+  }
+  
+  function clearCanvas(ctx) {
+    var startX = 0;
+    var startY = 0;
+    ctx.clearRect(startX, startY, canvaxWidth, canvasHeight);
+  }
+  
+  function draw(){
     
   }
+  
+  function randomRange (min, max) {
+    return Math.floor(Math.random() * (max + 1 - min)) + min;
+}
 });
