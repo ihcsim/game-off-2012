f574e06ee4e5ed72ce58a098e73daa8f95a2b1f8
Ivan Sim <ihcsim@gmail.com>
2012-11-03 19:27:51 -0700

    Add 'direction' to player object. Include unit tests.

--- js/models/Player.js
+++ js/models/Player.js
@@ -1,66 +1,91 @@
-Player = function (spriteSrc){
-  var DEFAULT_SPRITE_POS_X = 0;
-  var DEFAULT_SPRITE_POS_Y = 600;
-  var DEFAULT_PLAYER_HEIGHT = 54;
-  var DEFAULT_PLAYER_WIDTH = 35;
-  var DEFAULT_START_POS_X = 400;
-  var DEFAULT_START_POS_Y = 300;
-  var DEFAULT_MOVEMENT_SPEED = 2; // pixels
+function Player(spriteSrc, stage){
+  this.srcX = 0; // pos x in sprite image
+  this.srcY = 600; // pos y in sprite image
+  this.width = 35;
+  this.height = 54;
+  this.drawX = 400; // start pos x
+  this.drawY = 300; // start pos y
+  this.centerX = this.drawX + (this.width / 2);
+  this.centerY = this.drawY + (this.height / 2);
+  this.speed = 2;
+  
+  this.isUpKey = false;
+  this.isRightKey = false;
+  this.isDownKey = false;
+  this.isLeftKey = false;
+  this.isSpacebar = false;
+  
+  this.stage = stage;
   
   this.sprite = new Image();
   this.sprite.src = spriteSrc;
   
-  this.srcX = DEFAULT_SPRITE_POS_X;
-  this.srcY = DEFAULT_SPRITE_POS_Y;
-  this.width = DEFAULT_PLAYER_WIDTH;
-  this.height = DEFAULT_PLAYER_HEIGHT;
-  this.posX = DEFAULT_START_POS_X;
-  this.posY = DEFAULT_START_POS_Y;
-  this.centerX = this.posX + (this.width / 2);
-  this.centerY = this.posY + (this.height / 2);
-  this.speed = DEFAULT_MOVEMENT_SPEED;
+  this.direction = new Direction();
+  this.currentDirection = this.direction.SOUTH;
+}
+
+Player.prototype.isFacingNorth = function(){
+  return this.currentDirection == this.direction.NORTH;
 };
 
-Player.prototype.Direction = {
-  NORTH: "North",
-  SOUTH: "South",
-  EAST: "East",
-  WEST: "West"
+Player.prototype.isFacingSouth = function(){
+  return this.currentDirection == this.direction.SOUTH;
 };
+
+Player.prototype.isFacingEast = function(){
+  return this.currentDirection == this.direction.EAST;
+};
+
+Player.prototype.isFacingWest = function(){
+  return this.currentDirection == this.direction.WEST;
+};
+
+Player.prototype.executeAction = function(event){
+  var keyID = event.keyCode || event.which;
+  if(keyID == 38)
+    this.currentDirection = this.direction.NORTH;
+  else if(keyID == 40)
+    this.currentDirection = this.direction.SOUTH;
+  else if(keyID == 39)
+    this.currentDirection = this.direction.EAST;
+  else if(keyID == 37)
+    this.currentDirection = this.direction.WEST;
+};
+
 
 Player.prototype.updatePos = function () {
-  this.centerX = this.posX + (this.width / 2);
-  this.centerY = this.posY + (this.height / 2);
+  this.centerX = this.drawX + (this.width / 2);
+  this.centerY = this.drawY + (this.height / 2);
   this.checkDirection();
 };
 
 Player.prototype.draw = function () {
-  this.stage.drawImage(imgSprite, this.srcX, this.srcY, this.width, this.height, this.posX, this.posY, this.width, this.height);
+  this.stage.drawImage(imgSprite, this.srcX, this.srcY, this.width, this.height, this.drawX, this.drawY, this.width, this.height);
 };
 
 Player.prototype.checkDirection = function () {
-  var newposX = this.posX,
-      newposY = this.posY,
+  var newDrawX = this.drawX,
+      newDrawY = this.drawY,
       obstacleCollision = false;
   if (this.isUpKey) {
-      newposY -= this.speed;
+      newDrawY -= this.speed;
       this.srcX = 35; // Facing north
   } else if (this.isDownKey) {
-      newposY += this.speed;
+      newDrawY += this.speed;
       this.srcX = 0; // Facing south
   } else if (this.isRightKey) {
-      newposX += this.speed;
+      newDrawX += this.speed;
       this.srcX = 105; // Facing east
   } else if (this.isLeftKey) {
-      newposX -= this.speed;
+      newDrawX -= this.speed;
       this.srcX = 70; // Facing west
   }
 
-  //obstacleCollision = this.checkObstacleCollide(newposX, newposY);
+  //obstacleCollision = this.checkObstacleCollide(newDrawX, newDrawY);
 
-  if (!obstacleCollision && !outOfBounds(this, newposX, newposY)) {
-      this.posX = newposX;
-      this.posY = newposY;
+  if (!obstacleCollision && !outOfBounds(this, newDrawX, newDrawY)) {
+      this.drawX = newDrawX;
+      this.drawY = newDrawY;
   }
 };
 
---
+++ test/test-cases/TestPlayerDirection.js
@@ -0,0 +1,31 @@
+test("Player - Facing north on-up-arrow key", function() {
+  var event = jQuery.Event("keydown", {keyCode:38});
+  var spriteSrc = "../images/sprite.png";
+  var player = new Player(spriteSrc);
+  player.executeAction(event);
+  ok(player.isFacingNorth(), "Player is not facing north on-up-arrow key");
+});
+
+test("Player - Facing south on-down-arrow key", function() {
+  var event = jQuery.Event("keydown", {keyCode:40});
+  var spriteSrc = "../images/sprite.png";
+  var player = new Player(spriteSrc);
+  player.executeAction(event);
+  ok(player.isFacingSouth(), "Player is not facing south on-down-arrow key");
+});
+
+test("Player - Facing east on-right-arrow key", function() {
+  var event = jQuery.Event("keydown", {keyCode:39});
+  var spriteSrc = "../images/sprite.png";
+  var player = new Player(spriteSrc);
+  player.executeAction(event);
+  ok(player.isFacingEast(), "Player is not facing east on-down-arrow key");
+});
+
+test("Player - Facing west on-left-arrow key", function() {
+  var event = jQuery.Event("keydown", {keyCode:37});
+  var spriteSrc = "../images/sprite.png";
+  var player = new Player(spriteSrc);
+  player.executeAction(event);
+  ok(player.isFacingWest(), "Player is not facing west on-right-arrow key");
+});
