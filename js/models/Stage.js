019ec4cecadac0e42f63b2e37197da8080d4a97f
Ivan Sim <ihcsim@gmail.com>
2012-11-04 19:51:38 -0800

    Promote position-related local variables to fields.

--- js/models/Stage.js
+++ js/models/Stage.js
@@ -1,4 +1,14 @@
 Stage = function(spriteSrc, dom){
+  var DEFAULT_SPRITE_POS_X = 0;
+  var DEFAULT_SPRITE_POS_Y = 0;
+  var DEFAULT_START_POS_X = 0;
+  var DEFAULT_START_POS_Y = 0;
+  
+  this.srcX = DEFAULT_SPRITE_POS_X;
+  this.srcY = DEFAULT_SPRITE_POS_Y;
+  this.drawX = DEFAULT_START_POS_X;
+  this.drawY = DEFAULT_START_POS_Y;
+  
   this.sprite = new Image();
   this.sprite.src = spriteSrc;
   
@@ -12,10 +22,6 @@ Stage = function(spriteSrc, dom){
   
   this.loadBackground = function(){
     var stageCtx = this.dom.getContext("2d");
-    var imgStartX = 0;
-    var imgStartY = 0;
-    var stageStartX = 0;
-    var stageStartY = 0;
-    stageCtx.drawImage(this.sprite, imgStartX, imgStartY, this.width, this.height, stageStartX, stageStartY, this.width, this.height);
+    stageCtx.drawImage(this.sprite, this.srcX, this.srcY, this.width, this.height, this.drawX, this.drawY, this.width, this.height);
   };
 };
