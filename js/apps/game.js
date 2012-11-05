b62aa60697a8023b39e15fd9dd7dc5d88707266f
Ivan Sim <ihcsim@gmail.com>
2012-11-04 20:04:38 -0800

    Remove unused function in game.js.

--- js/apps/game.js
+++ js/apps/game.js
@@ -63,24 +63,4 @@ $(document).ready(function(){
     stageCtx.drawImage(newStage.sprite, newStage.srcX, newStage.srcY, newStage.width, newStage.height, newStage.posX, newStage.posY, newStage.width, newStage.height);
     stageCanvasCtx.drawImage(player.sprite, player.srcX, player.srcY, player.width, player.height, player.posX, player.posY, player.width, player.height);
   }
-
-  function outOfBounds(a, x, y) {
-    var newBottomY = y + a.height,
-        newTopY = y,
-        newRightX = x + a.width,
-        newLeftX = x,
-        treeLineTop = 5,
-        treeLineBottom = 570,
-        treeLineRight = 750,
-        treeLineLeft = 65;
-    return newBottomY > treeLineBottom ||
-        newTopY < treeLineTop ||
-        newRightX > treeLineRight ||
-        newLeftX < treeLineLeft;
-  }
-  
-  
-  function randomRange (min, max) {
-    return Math.floor(Math.random() * (max + 1 - min)) + min;
-  }
 });
