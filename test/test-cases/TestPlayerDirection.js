aea417f318abef375d985bfa488a90d7563b9e29
Ivan Sim <ihcsim@gmail.com>
2012-11-10 22:19:51 -0800

    Change tests title.

--- test/test-cases/TestPlayerDirection.js
+++ test/test-cases/TestPlayerDirection.js
@@ -1,4 +1,4 @@
-test("Player - Facing north on-up-arrow key", function() {
+test("Player Direction- Facing north on-up-arrow key", function() {
   var event = jQuery.Event("keydown", {keyCode:38});
   var spriteSrc = "../images/sprite.png";
   var player = new Player(spriteSrc);
@@ -6,7 +6,7 @@ test("Player - Facing north on-up-arrow key", function() {
   ok(player.isFacingNorth(), "Player is not facing north on-up-arrow key");
 });
 
-test("Player - Facing south on-down-arrow key", function() {
+test("Player Direction - Facing south on-down-arrow key", function() {
   var event = jQuery.Event("keydown", {keyCode:40});
   var spriteSrc = "../images/sprite.png";
   var player = new Player(spriteSrc);
@@ -14,7 +14,7 @@ test("Player - Facing south on-down-arrow key", function() {
   ok(player.isFacingSouth(), "Player is not facing south on-down-arrow key");
 });
 
-test("Player - Facing east on-right-arrow key", function() {
+test("Player Direction - Facing east on-right-arrow key", function() {
   var event = jQuery.Event("keydown", {keyCode:39});
   var spriteSrc = "../images/sprite.png";
   var player = new Player(spriteSrc);
@@ -22,7 +22,7 @@ test("Player - Facing east on-right-arrow key", function() {
   ok(player.isFacingEast(), "Player is not facing east on-down-arrow key");
 });
 
-test("Player - Facing west on-left-arrow key", function() {
+test("Player Direction - Facing west on-left-arrow key", function() {
   var event = jQuery.Event("keydown", {keyCode:37});
   var spriteSrc = "../images/sprite.png";
   var player = new Player(spriteSrc);
