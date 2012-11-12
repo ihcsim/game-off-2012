efdf3635853bcec20dbceeb8717b89e7ba1773bc
Ivan Sim <ihcsim@gmail.com>
2012-11-11 22:42:34 -0800

    Move draw and clear functions to Stage object.

--- js/apps/game/draw-engine.js
+++ js/apps/game/draw-engine.js
@@ -1,15 +1,3 @@
-var stage = $("#stage");
-var stageCtx = stage[0].getContext("2d");
-function drawStage(stage){
-  stageCtx.drawImage(stage.sprite, stage.srcX, stage.srcY, stage.dimension.width, stage.dimension.height, stage.position.posX, stage.position.posY, stage.dimension.width, stage.dimension.height);
-}
-
-var stageCanvas = $("#stage-canvas");
-var stageCanvasCtx = stageCanvas[0].getContext("2d");
-function clearStage(stage) {
-  stageCanvasCtx.clearRect(stage.srcX, stage.srcY, stage.width, stage.height);
-}
-
 function outOfBounds(player) {
   var topEdge = 5;
   var bottomEdge = 570;
