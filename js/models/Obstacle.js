8d332d6bc7c11b6ea046b92bf53a1f7a3953df4f
Ivan Sim <ihcsim@gmail.com>
2012-11-11 23:05:49 -0800

    Fix incorrect function name error.

--- js/models/Obstacle.js
+++ js/models/Obstacle.js
@@ -1,5 +1,5 @@
 Obstacle = function(srcX, srcY, width, height){
   this.position = new Coordinates(srcX, srcY);
-  this.dimension = new Dimension(width, height)
+  this.dimension = new Dimension(width, height);
   this.coordinates = calculateRectCoordinates(this.position, this.dimension);
 };
