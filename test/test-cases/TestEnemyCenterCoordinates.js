13ca01ceca56876a0ef9377a05f5c2b3606f2fe0
Ivan Sim <ihcsim@gmail.com>
2012-11-11 22:10:03 -0800

    Update enemy center coordinates unit tests to access Dimension object width and height.

--- test/test-cases/TestEnemyCenterCoordinates.js
+++ test/test-cases/TestEnemyCenterCoordinates.js
@@ -2,7 +2,7 @@ test("Enemy position - Calculate enemy center X", function() {
   var spriteSrc = "../images/sprite.png";
   var enemy = new Enemy(spriteSrc);
   enemy.posX = 0;
-  enemy.width = 200;
+  enemy.dimension.width = 200;
   var centerX =  enemy.calculateCenterX();
   ok(centerX == 100, "Enemy center X coordinate is incorrect");
 });
@@ -11,7 +11,7 @@ test("Enemy position - Calculate enemy center Y", function() {
   var spriteSrc = "../images/sprite.png";
   var enemy = new Enemy(spriteSrc);
   enemy.posY = 100;
-  enemy.height = 200;
+  enemy.dimension.height = 200;
   var centerX =  enemy.calculateCenterY();
   ok(centerX == 200, "Enemy center Y coordinate is incorrect");
 });
