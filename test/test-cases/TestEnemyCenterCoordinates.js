f7c040adc4810ca36127bb536036f7c662317a71
Ivan Sim <ihcsim@gmail.com>
2012-11-14 17:25:58 -0800

    Change unit tests title.

--- test/test-cases/TestEnemyCenterCoordinates.js
+++ test/test-cases/TestEnemyCenterCoordinates.js
@@ -1,4 +1,4 @@
-test("Enemy position - Calculate enemy center X", function() {
+test("Enemy center position - Calculate enemy center X", function() {
   var spriteSrc = "../images/sprite.png";
   var enemy = new Enemy(spriteSrc);
   enemy.currentPosition = new Coordinates(0, 0);
@@ -7,7 +7,7 @@ test("Enemy position - Calculate enemy center X", function() {
   ok(centerCoordinates.posX == 100, "Enemy center X coordinate is incorrect");
 });
 
-test("Enemy position - Calculate enemy center Y", function() {
+test("Enemy center position - Calculate enemy center Y", function() {
   var spriteSrc = "../images/sprite.png";
   var enemy = new Enemy(spriteSrc);
   enemy.currentPosition = new Coordinates(100, 100);
