2f3d59549ab1de814dc163293d9262f7a22a3376
Ivan Sim <ihcsim@gmail.com>
2012-11-14 20:01:35 -0800

    Fix enemy movement issue.

--- js/models/Enemy.js
+++ js/models/Enemy.js
@@ -4,6 +4,11 @@ Enemy = function(spriteSrc){
   var DEFAULT_ENEMY_WIDTH = 45;
   var DEFAULT_ENEMY_HEIGHT = 54;
   var DEFAULT_SPEED = 1; // pixel
+  var DEFAULT_MIN_TIME = 4000; // seconds
+  var DEFAULT_MAX_TIME = 10000; // seconds
+  var DEFAULT_MOVEMENT_RADIUS = 50; // pixels
+  var DEFAULT_STAGE_WIDTH = 800;
+  var DEFAULT_STAGE_HEIGHT = 600;
   
   this.srcX = DEFAULT_SPRITE_POS_X;
   this.srcY = DEFAULT_SPRITE_POS_Y;
@@ -14,20 +19,77 @@ Enemy = function(spriteSrc){
   this.isDead = false;
 
   var minX = 0;
-  var maxX = 800 - this.dimension.width;
+  var maxX = DEFAULT_STAGE_WIDTH - this.dimension.width;
   var minY = 0;
-  var maxY = 600 - this.dimension.height;
-  this.currentPosition = generateRandomCoordinates(minX, maxX, minY, maxY);
+  var maxY = DEFAULT_STAGE_HEIGHT - this.dimension.height;
+
+  var initCoords = generateRandomCoordinates(minX, maxX, minY, maxY);
+  this.currentPosition = new Coordinates(initCoords.posX, initCoords.posY);
+  
   this.calculateCenterCoordinates = function(){
     return calculateCenterCoordinates(this.currentPosition, this.dimension);
   };
+  this.nextPosition = this.calculateCenterCoordinates();
   
+  this.nextMoveTime = rand(DEFAULT_MIN_TIME, DEFAULT_MAX_TIME);
+  var thisEnemy = this;
+  this.moveInterval = setInterval(function(){ thisEnemy.setNextRandomPosition();}, thisEnemy.nextMoveTime);
+  
+  this.setNextRandomPosition = function(){
+    this.nextMoveTime = rand(DEFAULT_MIN_TIME, DEFAULT_MAX_TIME);
+    
+    var centerCoords = this.calculateCenterCoordinates();
+    var minPosX = centerCoords.posX - DEFAULT_MOVEMENT_RADIUS;
+    var maxPosX = centerCoords.posX + DEFAULT_MOVEMENT_RADIUS;
+    var minPosY = centerCoords.posY - DEFAULT_MOVEMENT_RADIUS;
+    var maxPosY = centerCoords.posY + DEFAULT_MOVEMENT_RADIUS;
+    
+    if(minPosX < 0)
+      minPosX = 0;
+    
+    if(maxPosX > DEFAULT_STAGE_WIDTH)
+      maxPosX = DEFAULT_STAGE_WIDTH;
+    
+    if(minPosY < 0)
+      minPosY = 0;
+    
+    if(maxPosY > DEFAULT_STAGE_HEIGHT)
+      maxPosY = DEFAULT_STAGE_HEIGHT;
+    
+    var nextPosX = rand(minPosX, maxPosX);
+    var nextPosY = rand(minPosY, maxPosY);
+    this.nextPosition = new Coordinates(nextPosX, nextPosY);
+  };
+
   this.sprite = new Image();
   this.sprite.src = spriteSrc;
   
   this.updatePosition = function () {
-    this.calculateCenterCoordinates();
+    var centerCoords = this.calculateCenterCoordinates();
+    var newPosX = this.currentPosition.posX;
+    var newPosY = this.currentPosition.posY;
+
+    if (centerCoords.posX < this.nextPosition.posX)
+      newPosX = this.currentPosition.posX + this.speed;
+    else if (centerCoords.posX > this.nextPosition.posX)
+      newPosX = this.currentPosition.posX - this.speed;
+    
+    if (centerCoords.posY < this.nextPosition.posY)
+      newPosY = this.currentPosition.posY + this.speed;
+    else if (centerCoords.posY > this.nextPosition.posY)
+      newPosY = this.currentPosition.posY  - this.speed;
+    
+    this.currentPosition = new Coordinates(newPosX, newPosY);
   };
+  
+  this.die = function () {
+    var soundEffect = new Audio("audio/dying.wav");
+    soundEffect.play();
+    clearInterval(this.moveInterval);
+    this.srcX = 185;
+    this.isDead = true;
+  };
+
   
   this.draw = function() {
     var stageCanvas = $("#stage-canvas");
@@ -37,5 +99,5 @@ Enemy = function(spriteSrc){
         this.dimension.width, this.dimension.height, 
         this.currentPosition.posX, this.currentPosition.posY, 
         this.dimension.width, this.dimension.height);
-  }
+  };
 };
--- test/test-cases/TestEnemyNextPosition.js
+++ test/test-cases/TestEnemyNextPosition.js
@@ -4,8 +4,8 @@ test("Enemy next random position test #1- X-coordinate is within radius", functi
   enemy.currentPosition = new Coordinates(150, 150);
   enemy.dimension = new Dimension(0, 0);
 
-  var nextPositionCoordinates = enemy.setNextRandomPosition();
-  ok(nextPositionCoordinates.posX >= 100 && nextPositionCoordinates.posX <= 200, "Enemy next position X coordinate is outside of the expected radius");
+  enemy.setNextRandomPosition();
+  ok(enemy.nextPosition.posX >= 100 && enemy.nextPosition.posX <= 200, "Enemy next position X coordinate is outside of the expected radius");
 });
 
 test("Enemy next random position test #1- Y-coordinate is within radius", function() {
@@ -14,8 +14,8 @@ test("Enemy next random position test #1- Y-coordinate is within radius", functi
   enemy.currentPosition = new Coordinates(150, 150);
   enemy.dimension = new Dimension(0, 0);
   
-  var nextPositionCoordinates = enemy.setNextRandomPosition();
-  ok(nextPositionCoordinates.posY >= 100 && nextPositionCoordinates.posY <= 200, "Enemy next position X coordinate is outside of the expected radius");
+  enemy.setNextRandomPosition();
+  ok(enemy.nextPosition.posY >= 100 && enemy.nextPosition.posY <= 200, "Enemy next position Y coordinate is outside of the expected radius");
 });
 
 test("Enemy next random position test #2- X-coordinate is within radius", function() {
@@ -24,8 +24,8 @@ test("Enemy next random position test #2- X-coordinate is within radius", functi
   enemy.currentPosition = new Coordinates(800, 600);
   enemy.dimension = new Dimension(0, 0);
 
-  var nextPositionCoordinates = enemy.setNextRandomPosition();
-  ok(nextPositionCoordinates.posX >= 750 && nextPositionCoordinates.posX <= 800, "Enemy next position X coordinate is outside of the expected radius");
+  enemy.setNextRandomPosition();
+  ok(enemy.nextPosition.posX >= 750 && enemy.nextPosition.posX <= 800, "Enemy next position X coordinate is outside of the expected radius");
 });
 
 test("Enemy next random position test #2- Y-coordinate is within radius", function() {
@@ -34,22 +34,63 @@ test("Enemy next random position test #2- Y-coordinate is within radius", functi
   enemy.currentPosition = new Coordinates(800, 600);
   enemy.dimension = new Dimension(0, 0);
   
-  var nextPositionCoordinates = enemy.setNextRandomPosition();
-  ok(nextPositionCoordinates.posY >= 550 && nextPositionCoordinates.posY <= 600, "Enemy next position X coordinate is outside of the expected radius");
+  enemy.setNextRandomPosition();
+  ok(enemy.nextPosition.posY >= 550 && enemy.nextPosition.posY <= 600, "Enemy next position Y coordinate is outside of the expected radius");
 });
 
 test("Enemy next random position test #3- X-coordinate is within radius", function() {
   var spriteSrc = "../images/sprite.png";
   var enemy = new Enemy(spriteSrc);
 
-  var nextPositionCoordinates = enemy.setNextRandomPosition();
-  ok(nextPositionCoordinates.posX >= 0 && nextPositionCoordinates.posX <= 800, "Enemy next position X coordinate is outside of the expected radius");
+  enemy.setNextRandomPosition();
+  ok(enemy.nextPosition.posX >= 0 && enemy.nextPosition.posX <= 800, "Enemy next position X coordinate is outside of the expected radius");
 });
 
 test("Enemy next random position test #3- Y-coordinate is within radius", function() {
   var spriteSrc = "../images/sprite.png";
   var enemy = new Enemy(spriteSrc);
 
-  var nextPositionCoordinates = enemy.setNextRandomPosition();
-  ok(nextPositionCoordinates.posY >= 0 && nextPositionCoordinates.posY <= 600, "Enemy next position X coordinate is outside of the expected radius");
+  enemy.setNextRandomPosition();
+  ok(enemy.nextPosition.posY >= 0 && enemy.nextPosition.posY <= 600, "Enemy next position Y coordinate is outside of the expected radius");
+});
+
+
+test("Enemy update position test #1- X-coordinate increased by 1", function() {
+  var spriteSrc = "../images/sprite.png";
+  var enemy = new Enemy(spriteSrc);
+  enemy.currentPosition = new Coordinates(100, 200);
+  enemy.nextPosition = new Coordinates(150, 250);
+  enemy.updatePosition();
+
+  ok(enemy.currentPosition.posX == 101, "Enemy updated position X coordinate is supposed to increase by 1");
+});
+
+test("Enemy update position test #1- Y-coordinate increased by 1", function() {
+  var spriteSrc = "../images/sprite.png";
+  var enemy = new Enemy(spriteSrc);
+  enemy.currentPosition = new Coordinates(100, 200);
+  enemy.nextPosition = new Coordinates(150, 250);
+  enemy.updatePosition();
+
+  ok(enemy.currentPosition.posY == 201, "Enemy updated position Y coordinate is supposed to increase by 1");
+});
+
+test("Enemy update position test #1- X-coordinate increased by 1", function() {
+  var spriteSrc = "../images/sprite.png";
+  var enemy = new Enemy(spriteSrc);
+  enemy.currentPosition = new Coordinates(101, 201);
+  enemy.nextPosition = new Coordinates(150, 250);
+  enemy.updatePosition();
+
+  ok(enemy.currentPosition.posX == 102, "Enemy updated position X coordinate is supposed to increase by 1");
+});
+
+test("Enemy update position test #1- Y-coordinate increased by 1", function() {
+  var spriteSrc = "../images/sprite.png";
+  var enemy = new Enemy(spriteSrc);
+  enemy.currentPosition = new Coordinates(101, 201);
+  enemy.nextPosition = new Coordinates(150, 250);
+  enemy.updatePosition();
+
+  ok(enemy.currentPosition.posY == 202, "Enemy updated position Y coordinate is supposed to increase by 1");
 });
