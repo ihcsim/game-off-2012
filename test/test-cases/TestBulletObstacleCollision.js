72212ca4fe437c9ecc1c2e2d0bff2e04ef5b7063
Ivan Sim <ihcsim@gmail.com>
2012-11-17 18:06:37 -0800

    Refactor bullet/obstacle collision unit tests to use coordinates-calculator to calculate rectangular midpoint coordinates.

--- test/test-cases/TestBulletObstacleCollision.js
+++ test/test-cases/TestBulletObstacleCollision.js
@@ -1,48 +1,52 @@
 test("Bullet/Obstacle Collision - Bullet collides with obstacle top left corner", function() {
-  var bullet = new Bullet();
-  bullet.currentPosition = new Coordinates(20, 20);
- 
   var obstaclePosX = 20;
   var obstaclePosY = 20;
   var obstacleWidth = 20;
   var obstacleHeight = 20;
   var obstacle = new Obstacle(obstaclePosX, obstaclePosY, obstacleWidth, obstacleHeight);
+  
+  var bullet = new Bullet();
+  var obstacleRectCoordinates = calculateRectCoordinates(obstacle.position, obstacle.dimension);
+  bullet.currentPosition = new Coordinates(obstacleRectCoordinates.topLeft.posX, obstacleRectCoordinates.topLeft.posY);
   ok(bulletHitObstacle(bullet, obstacle), "A collision should occur");
 });
 
 test("Bullet/Obstacle Collision - Bullet collides with obstacle top right corner", function() {
-  var bullet = new Bullet();
-  bullet.currentPosition = new Coordinates(40, 20);
- 
   var obstaclePosX = 20;
   var obstaclePosY = 20;
   var obstacleWidth = 20;
   var obstacleHeight = 20;
   var obstacle = new Obstacle(obstaclePosX, obstaclePosY, obstacleWidth, obstacleHeight);
+  
+  var bullet = new Bullet();
+  var obstacleRectCoordinates = calculateRectCoordinates(obstacle.position, obstacle.dimension);
+  bullet.currentPosition = new Coordinates(obstacleRectCoordinates.topRight.posX, obstacleRectCoordinates.topRight.posY);
   ok(bulletHitObstacle(bullet, obstacle), "A collision should occur");
 });
 
 test("Bullet/Obstacle Collision - Bullet collides with obstacle bottom left corner", function() {
-  var bullet = new Bullet();
-  bullet.currentPosition = new Coordinates(20, 40);
- 
   var obstaclePosX = 20;
   var obstaclePosY = 20;
   var obstacleWidth = 20;
   var obstacleHeight = 20;
   var obstacle = new Obstacle(obstaclePosX, obstaclePosY, obstacleWidth, obstacleHeight);
+  
+  var bullet = new Bullet();
+  var obstacleRectCoordinates = calculateRectCoordinates(obstacle.position, obstacle.dimension);
+  bullet.currentPosition = new Coordinates(obstacleRectCoordinates.bottomLeft.posX, obstacleRectCoordinates.bottomLeft.posY);
   ok(bulletHitObstacle(bullet, obstacle), "A collision should occur");
 });
 
 test("Bullet/Obstacle Collision - Bullet collides with obstacle bottom right corner", function() {
-  var bullet = new Bullet();
-  bullet.currentPosition = new Coordinates(40, 40);
- 
   var obstaclePosX = 20;
   var obstaclePosY = 20;
   var obstacleWidth = 20;
   var obstacleHeight = 20;
   var obstacle = new Obstacle(obstaclePosX, obstaclePosY, obstacleWidth, obstacleHeight);
+  
+  var bullet = new Bullet();
+  var obstacleRectCoordinates = calculateRectCoordinates(obstacle.position, obstacle.dimension);
+  bullet.currentPosition = new Coordinates(obstacleRectCoordinates.bottomRight.posX, obstacleRectCoordinates.bottomRight.posY);
   ok(bulletHitObstacle(bullet, obstacle), "A collision should occur");
 });
 
@@ -60,26 +64,30 @@ test("Bullet/Obstacle Collision - Bullet collides with obstacle center", functio
 });
 
 test("Bullet/Obstacle Collision - Bullet collides with obstacle left boundary mid-point", function() {
-  var bullet = new Bullet();
-  bullet.currentPosition = new Coordinates(20, 30);
- 
   var obstaclePosX = 20;
   var obstaclePosY = 20;
   var obstacleWidth = 20;
   var obstacleHeight = 20;
   var obstacle = new Obstacle(obstaclePosX, obstaclePosY, obstacleWidth, obstacleHeight);
+  
+  var bullet = new Bullet();
+  var obstacleRectCoordinates = calculateRectCoordinates(obstacle.position, obstacle.dimension);
+  var midpointCoordinates = calculateMidPointCoordinates(obstacleRectCoordinates.topLeft, obstacleRectCoordinates.bottomLeft);
+  bullet.currentPosition = new Coordinates(midpointCoordinates.posX, midpointCoordinates.posY);
   ok(bulletHitObstacle(bullet, obstacle), "A collision should occur");
 });
 
 test("Bullet/Obstacle Collision - Bullet collides with obstacle right boundary mid-point", function() {
-  var bullet = new Bullet();
-  bullet.currentPosition = new Coordinates(40, 30);
- 
   var obstaclePosX = 20;
   var obstaclePosY = 20;
   var obstacleWidth = 20;
   var obstacleHeight = 20;
   var obstacle = new Obstacle(obstaclePosX, obstaclePosY, obstacleWidth, obstacleHeight);
+  
+  var bullet = new Bullet();
+  var obstacleRectCoordinates = calculateRectCoordinates(obstacle.position, obstacle.dimension);
+  var midpointCoordinates = calculateMidPointCoordinates(obstacleRectCoordinates.topRight, obstacleRectCoordinates.bottomRight);
+  bullet.currentPosition = new Coordinates(midpointCoordinates.posX, midpointCoordinates.posY);
   ok(bulletHitObstacle(bullet, obstacle), "A collision should occur");
 });
 
