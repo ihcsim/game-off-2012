e53a3cfae1b7b5be2756c9ba0f27e22687fb6367
Ivan Sim <ihcsim@gmail.com>
2012-11-17 16:53:31 -0800

    Add more unit tests for bullet/obstacle collision codes.

--- test/test-cases/TestBulletObstacleCollision.js
+++ test/test-cases/TestBulletObstacleCollision.js
@@ -59,9 +59,45 @@ test("Bullet/Obstacle Collision - Bullet collides with obstacle center", functio
   ok(bulletHitObstacle(bullet, obstacle), "A collision should occur");
 });
 
+test("Bullet/Obstacle Collision - Bullet collides with obstacle left boundary mid-point", function() {
+  var bullet = new Bullet();
+  bullet.currentPosition = new Coordinates(20, 30);
+ 
+  var obstaclePosX = 20;
+  var obstaclePosY = 20;
+  var obstacleWidth = 20;
+  var obstacleHeight = 20;
+  var obstacle = new Obstacle(obstaclePosX, obstaclePosY, obstacleWidth, obstacleHeight);
+  ok(bulletHitObstacle(bullet, obstacle), "A collision should occur");
+});
+
+test("Bullet/Obstacle Collision - Bullet collides with obstacle right boundary mid-point", function() {
+  var bullet = new Bullet();
+  bullet.currentPosition = new Coordinates(40, 30);
+ 
+  var obstaclePosX = 20;
+  var obstaclePosY = 20;
+  var obstacleWidth = 20;
+  var obstacleHeight = 20;
+  var obstacle = new Obstacle(obstaclePosX, obstaclePosY, obstacleWidth, obstacleHeight);
+  ok(bulletHitObstacle(bullet, obstacle), "A collision should occur");
+});
+
 test("Bullet/Obstacle Collision - Bullet misses", function() {
   var bullet = new Bullet();
   bullet.currentPosition = new Coordinates(10, 40);
+ 
+  var obstaclePosX = 20;
+  var obstaclePosY = 20;
+  var obstacleWidth = 20;
+  var obstacleHeight = 20;
+  var obstacle = new Obstacle(obstaclePosX, obstaclePosY, obstacleWidth, obstacleHeight);
+  ok(!bulletHitObstacle(bullet, obstacle), "No collisions should occur");
+});
+
+test("Bullet/Obstacle Collision - Bullet misses", function() {
+  var bullet = new Bullet();
+  bullet.currentPosition = new Coordinates(40, 45);
  
   var obstaclePosX = 20;
   var obstaclePosY = 20;
