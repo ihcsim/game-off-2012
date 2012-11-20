1c51660078a03e34bacca292dca4a5ee417c61e0
Ivan Sim <ihcsim@gmail.com>
2012-11-19 19:32:23 -0800

    Promote obstacle heights and widths local variables to constants.

--- js/apps/game/factory.js
+++ js/apps/game/factory.js
@@ -29,21 +29,20 @@ function initBullet(){
   return new Bullet();
 }
 
+var OBSTACLE_TREE_WIDTH = 65;
+var OBSTACLE_TREE_HEIGHT = 90;
+var OBSTACLE_ROCK_DIMENSIONS = 30;
+var OBSTACLE_BUSH_HEIGHT = 28;
 function initObstacles(){
-  var treeWidth = 65;
-  var treeHeight = 90;
-  var rockDimensions = 30;
-  var bushHeight = 28;
-
-  var obstacle1 = initObstacle(78, 360, treeWidth, treeHeight);
-  var obstacle2 = initObstacle(390, 395, treeWidth, treeHeight);
-  var obstacle3 = initObstacle(415, 102, treeWidth, treeHeight);
-  var obstacle4 = initObstacle(619, 184,treeWidth, treeHeight);
-  var obstacle5 = initObstacle(97, 63, rockDimensions, rockDimensions);
-  var obstacle6 = initObstacle(296, 379, rockDimensions, rockDimensions);
-  var obstacle7 = initObstacle(295, 25, 150, bushHeight);
-  var obstacle8 = initObstacle(570, 138, 150, bushHeight);
-  var obstacle9 = initObstacle(605, 492, 90, bushHeight);
+  var obstacle1 = initObstacle(78, 360, OBSTACLE_TREE_WIDTH, OBSTACLE_TREE_HEIGHT);
+  var obstacle2 = initObstacle(390, 395, OBSTACLE_TREE_WIDTH, OBSTACLE_TREE_HEIGHT);
+  var obstacle3 = initObstacle(415, 102, OBSTACLE_TREE_WIDTH, OBSTACLE_TREE_HEIGHT);
+  var obstacle4 = initObstacle(619, 184,OBSTACLE_TREE_WIDTH, OBSTACLE_TREE_HEIGHT);
+  var obstacle5 = initObstacle(97, 63, OBSTACLE_ROCK_DIMENSIONS, OBSTACLE_ROCK_DIMENSIONS);
+  var obstacle6 = initObstacle(296, 379, OBSTACLE_ROCK_DIMENSIONS, OBSTACLE_ROCK_DIMENSIONS);
+  var obstacle7 = initObstacle(295, 25, 150, OBSTACLE_BUSH_HEIGHT);
+  var obstacle8 = initObstacle(570, 138, 150, OBSTACLE_BUSH_HEIGHT);
+  var obstacle9 = initObstacle(605, 492, 90, OBSTACLE_BUSH_HEIGHT);
   var obstacles = [obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6, obstacle7, obstacle8, obstacle9];
   return obstacles;
 }
