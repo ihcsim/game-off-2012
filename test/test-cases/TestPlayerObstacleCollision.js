244be8302ec3ebeef2cd1e7f4fa4dd3a05a03446
Ivan Sim <ihcsim@gmail.com>
2012-11-19 19:40:25 -0800

    Refactor player/obstacle collision unit test to use factory function to create player and obstacle test fixtures.

--- test/test-cases/TestPlayerObstacleCollision.js
+++ test/test-cases/TestPlayerObstacleCollision.js
@@ -3,82 +3,52 @@ var DEFAULT_OBSTACLE_POSY = 20;
 var DEFAULT_OBSTACLE_WIDTH = 20;
 var DEFAULT_OBSTACLE_HEIGHT = 20;
 
+var player = initPlayer();
+var obstacle = initObstacle(DEFAULT_OBSTACLE_POSX, DEFAULT_OBSTACLE_POSY, DEFAULT_OBSTACLE_WIDTH, DEFAULT_OBSTACLE_HEIGHT);
 test("Player/Obstacle Collision - Player collides with obstacle top boundary", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.newPosition = new Coordinates(25, 20);
   player.dimension = new Dimension(0, 0);
-  
-  var obstacle = new Obstacle(DEFAULT_OBSTACLE_POSX, DEFAULT_OBSTACLE_POSY, DEFAULT_OBSTACLE_WIDTH, DEFAULT_OBSTACLE_HEIGHT);
   ok(playerWillCollideWithObstacle(player, obstacle), "A collision should occur");
 });
 
 test("Player/Obstacle Collision - Player collides with obstacle left boundary", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.newPosition = new Coordinates(25, 35);
   player.dimension = new Dimension(0, 0);
-
-  var obstacle = new Obstacle(DEFAULT_OBSTACLE_POSX, DEFAULT_OBSTACLE_POSY, DEFAULT_OBSTACLE_WIDTH, DEFAULT_OBSTACLE_HEIGHT);
   ok(playerWillCollideWithObstacle(player, obstacle), "A collision should occur");
 });
 
 test("Player/Obstacle Collision - Player collides with obstacle right boundary", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.newPosition = new Coordinates(30, 35);
   player.dimension = new Dimension(0, 0);
-
-  var obstacle = new Obstacle(DEFAULT_OBSTACLE_POSX, DEFAULT_OBSTACLE_POSY, DEFAULT_OBSTACLE_WIDTH, DEFAULT_OBSTACLE_HEIGHT);
   ok(playerWillCollideWithObstacle(player, obstacle), "A collision should occur");
 });
 
 test("Player/Obstacle Collision - Player collides with obstacle bottom boundary", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.newPosition = new Coordinates(25, 40);
   player.dimension = new Dimension(0, 0);
-
-  var obstacle = new Obstacle(DEFAULT_OBSTACLE_POSX, DEFAULT_OBSTACLE_POSY, DEFAULT_OBSTACLE_WIDTH, DEFAULT_OBSTACLE_HEIGHT);
   ok(playerWillCollideWithObstacle(player, obstacle), "A collision should occur");
 });
 
 test("Player/Obstacle Collision - No collision with obstacle top boundary", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.newPosition = new Coordinates(15, 0);
   player.dimension = new Dimension(0, 0);
-
-  var obstacle = new Obstacle(DEFAULT_OBSTACLE_POSX, DEFAULT_OBSTACLE_POSY, DEFAULT_OBSTACLE_WIDTH, DEFAULT_OBSTACLE_HEIGHT);
   ok(!playerWillCollideWithObstacle(player, obstacle), "A collision shouldn't occur");
 });
 
 test("Player/Obstacle Collision - No collision with obstacle left boundary", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.newPosition = new Coordinates(5, 35);
   player.dimension = new Dimension(0, 0);
-
-  var obstacle = new Obstacle(DEFAULT_OBSTACLE_POSX, DEFAULT_OBSTACLE_POSY, DEFAULT_OBSTACLE_WIDTH, DEFAULT_OBSTACLE_HEIGHT);
   ok(!playerWillCollideWithObstacle(player, obstacle), "A collision shouldn't occur");
 });
 
 test("Player/Obstacle Collision - No collision with obstacle right boundary", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.newPosition = new Coordinates(50, 35);
   player.dimension = new Dimension(0, 0);
-
-  var obstacle = new Obstacle(DEFAULT_OBSTACLE_POSX, DEFAULT_OBSTACLE_POSY, DEFAULT_OBSTACLE_WIDTH, DEFAULT_OBSTACLE_HEIGHT);
   ok(!playerWillCollideWithObstacle(player, obstacle), "A collision shouldn't occur");
 });
 
 test("Player/Obstacle Collision - No collision with obstacle bottom boundary", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.newPosition = new Coordinates(20, 45);
   player.dimension = new Dimension(0, 0);
-
-  var obstacle = new Obstacle(DEFAULT_OBSTACLE_POSX, DEFAULT_OBSTACLE_POSY, DEFAULT_OBSTACLE_WIDTH, DEFAULT_OBSTACLE_HEIGHT);
   ok(!playerWillCollideWithObstacle(player, obstacle), "A collision shouldn't occur");
 });
