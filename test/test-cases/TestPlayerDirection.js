5aef7b667d282c4210b1406feb41477b3380ea55
Ivan Sim <ihcsim@gmail.com>
2012-11-19 19:22:30 -0800

    Refactor player direction unit test to use factory function to create player test fixture.

--- test/test-cases/TestPlayerDirection.js
+++ test/test-cases/TestPlayerDirection.js
@@ -1,59 +1,44 @@
+var player = initPlayer();
 test("Player Direction- Face north", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.turnNorth();
   ok(player.isFacingNorth(), "Player is not facing north on-up-arrow key");
 });
 
 test("Player Direction - Face south", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.turnSouth();
   ok(player.isFacingSouth(), "Player is not facing south on-down-arrow key");
 });
 
 test("Player Direction - Face east", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.turnEast();
   ok(player.isFacingEast(), "Player is not facing east on-down-arrow key");
 });
 
 test("Player Direction - Face west", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.turnWest();
   ok(player.isFacingWest(), "Player is not facing west on-right-arrow key");
 });
 
 test("Player Direction- Facing north on-up-arrow key", function() {
   var event = jQuery.Event("keydown", {keyCode:38});
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.executeAction(event);
   ok(player.isFacingNorth(), "Player is not facing north on-up-arrow key");
 });
 
 test("Player Direction - Facing south on-down-arrow key", function() {
   var event = jQuery.Event("keydown", {keyCode:40});
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.executeAction(event);
   ok(player.isFacingSouth(), "Player is not facing south on-down-arrow key");
 });
 
 test("Player Direction - Facing east on-right-arrow key", function() {
   var event = jQuery.Event("keydown", {keyCode:39});
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.executeAction(event);
   ok(player.isFacingEast(), "Player is not facing east on-down-arrow key");
 });
 
 test("Player Direction - Facing west on-left-arrow key", function() {
   var event = jQuery.Event("keydown", {keyCode:37});
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.executeAction(event);
   ok(player.isFacingWest(), "Player is not facing west on-right-arrow key");
 });
