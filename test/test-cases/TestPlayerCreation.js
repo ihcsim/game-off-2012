6b09638ed734bec5e67c782da339d5b069e2c040
Ivan Sim <ihcsim@gmail.com>
2012-11-19 19:21:32 -0800

    Refactor unit test to use factory function to create player test fixture.

--- test/test-cases/TestPlayerCreation.js
+++ test/test-cases/TestPlayerCreation.js
@@ -1,17 +1,13 @@
+var player = initPlayer();
 test("Player - Create player", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   ok(player != null, "Failed to create player");
 });
 
 test("Player - Sprite image exists", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   ok(player.sprite.src != null, "Unable to locate sprite image resource");
 });
 
 test("Player - Sprite image file path matches", function() {
-  var expectedSpriteSrc = "file:///C:/dev/workspace/game-off-2012/images/sprite.png";
-  var player = new Player(expectedSpriteSrc);
+  var expectedSpriteSrc = "file:///C:/dev/workspace/game-off-2012/test/images/sprite.png";
   ok(player.sprite.src == expectedSpriteSrc, "Sprite image file paths don't match");
 });
