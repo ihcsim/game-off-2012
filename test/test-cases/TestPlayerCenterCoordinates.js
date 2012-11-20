33c92af62cb5266d07aedd0005031c1ccfba4a1a
Ivan Sim <ihcsim@gmail.com>
2012-11-19 19:38:05 -0800

    Refactor player-center-coordinates unit test to use factory function to create player test fixtures.

--- test/test-cases/TestPlayerCenterCoordinates.js
+++ test/test-cases/TestPlayerCenterCoordinates.js
@@ -1,6 +1,5 @@
+var player = initPlayer();
 test("Player center coordinates - Calculate player center coordinates #1", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.currentPosition = new Coordinates(10, 10);
   player.dimension = new Dimension(10, 20);
   var playerCenterCoord = player.calculateCenterCoordinates();
@@ -9,8 +8,6 @@ test("Player center coordinates - Calculate player center coordinates #1", funct
 });
 
 test("Player center coordinates - Calculate player center coordinates #2", function() {
-  var spriteSrc = "../images/sprite.png";
-  var player = new Player(spriteSrc);
   player.currentPosition = new Coordinates(10, 10);
   player.dimension = new Dimension(30, 50);
   var playerCenterCoord = player.calculateCenterCoordinates();
