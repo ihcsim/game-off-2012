367bd193cf13dce77d15f1c28cee4fa8b13e7910
Ivan Sim <ihcsim@gmail.com>
2012-11-19 19:24:06 -0800

    Refactor stage creation unit test to use factory function to create stage test fixture.

--- test/test-cases/TestStageCreation.js
+++ test/test-cases/TestStageCreation.js
@@ -1,29 +1,21 @@
+var stage = initStage();
 test("Stage - Build stage", function() {
-  var spriteSrc = "../images/sprite.png";
-  var stage = new Stage(spriteSrc);
   ok(stage != null, "Failed to build stage");
 });
 
 test("Stage - Sprite image exists", function() {
-  var spriteSrc = "../images/sprite.png";
-  var stage = new Stage(spriteSrc);
   ok(stage.sprite.src != null, "Unable to locate sprite image resource");
 });
 
 test("Stage - Sprite image file path matches", function() {
-  var expectedSpriteSrc = "file:///C:/dev/workspace/game-off-2012/images/sprite.png";
-  var stage = new Stage(expectedSpriteSrc);
+  var expectedSpriteSrc = "file:///C:/dev/workspace/game-off-2012/test/images/sprite.png";
   ok(stage.sprite.src == expectedSpriteSrc, "Sprite image file paths don't match");
 });
 
 test("Stage - Height is set", function() {
-  var spriteSrc = "../images/sprite.png";
-  var stage = new Stage(spriteSrc);
-  ok(stage.dimension.height == 600, "Sprite image file paths don't match");
+  ok(stage.dimension.height == 600, "Sprite image height don't match");
 });
 
 test("Stage - Width is set", function() {
-  var spriteSrc = "../images/sprite.png";
-  var stage = new Stage(spriteSrc);
-  ok(stage.dimension.width == 800, "Sprite image file paths don't match");
+  ok(stage.dimension.width == 800, "Sprite image width don't match");
 });
