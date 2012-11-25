3227da88d16f36b8e97e82c3201f68559902712b
Ivan Sim <ihcsim@gmail.com>
2012-11-24 17:00:42 -0800

    Add scorebox to main page. Update score-calculator to update scorebox on-kill.

--- js/apps/game/score-calculator.js
+++ js/apps/game/score-calculator.js
@@ -1,8 +1,10 @@
 var SCORE_PER_KILL = 200;
 var totalScore = 0;
+var scoreDOM = $("#score");
 
 function incrementScore(){
   totalScore += SCORE_PER_KILL;
+  scoreDOM.text(totalScore);
 }
 
 function viewTotalScore(){
