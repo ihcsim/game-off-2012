fd4e92fcc23b3d8dc2ab48f44aae31f77b98be9d
Ivan Sim <ihcsim@gmail.com>
2012-11-24 16:46:55 -0800

    Add more score calculation tests.

--- test/test-cases/TestScoreCalculation.js
+++ test/test-cases/TestScoreCalculation.js
@@ -1,3 +1,30 @@
+test("Score Calculator - Increment score once", function() {
+  incrementScore();
+  ok(viewTotalScore() == 200, "Score should be 200.");
+  resetTotalScore();
+});
+
+test("Score Calculator - Increment score twice", function() {
+  incrementScore();
+  incrementScore();
+  ok(viewTotalScore() == 400, "Score should be 400.");
+  resetTotalScore();
+});
+
+test("Score Calculator - Increment score six times", function() {
+  for(var index = 0; index < 6; index++)
+    incrementScore();
+  ok(viewTotalScore() == 1200, "Score should be 1200.");
+  resetTotalScore();
+});
+
+test("Score Calculator - Reset score after six increments", function() {
+  for(var index = 0; index < 6; index++)
+    incrementScore();
+  resetTotalScore();
+  ok(viewTotalScore() == 0, "Score should be 0.");
+});
+
 test("Score Calculator - Enemy on-dead event triggers score increments.", function() {
   initEnemyDiedEventHandler();
   var enemy = initEnemy();
